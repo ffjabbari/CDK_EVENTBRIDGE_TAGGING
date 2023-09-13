a0212ef515653f9379fee9574e439756a857cde6
nspacer <89945413+nspacer@users.noreply.github.com>
2023-09-13 20:22:55 +0200

    Add files via upload

--- lambda_code/lambda_function.py
+++ lambda_code/lambda_function.py
@@ -11,16 +11,16 @@ def lambda_handler(event, context):
     sns_client = boto3.client('sns')
 
     # Define the SNS topic ARN
-    sns_topic_arn = 'arn:aws:sns:us-east-1:123456789012:MySNSTopic'  # Replace with your SNS topic ARN
+    sns_topic_arn = 'arn:aws:sns:us-east-1:981241700645:MySNSTopic'  # Replace with your SNS topic ARN
 
     # Create a message to publish to the SNS topic
-    message = f"EC2 instance {instance_id} has been {event['detail']['eventName']}."
+    message = f"EC2 instance {instance_id} has been {event['detail']['state']}."
 
     # Publish the message to the SNS topic
     sns_client.publish(
         TopicArn=sns_topic_arn,
         Message=message,
-        Subject=f"EC2 Instance {instance_id} {event['detail']['eventName']} Event"
+        Subject=f"EC2 Instance {instance_id} {event['detail']['state']} Event"
     )
 
     return {
